6255b7ee393c839409bbd0f66ce8a31ac83c9c09
prajvalsudhir-deloitte <101543764+prajvalsudhir-deloitte@users.noreply.github.com>
2022-10-21 15:15:07 +0530

    base python app

---
+++ Python_Backend/app/app.py
@@ -0,0 +1,10 @@
+"""
+This is a placeholder file
+"""
+
+def hello():
+    """Method docstring goes here"""
+    print("Hello! Dr Fleet")
+
+if __name__ == "__main__":
+    hello()
---
+++ Python_Backend/app/process_video_to_frame.py
@@ -0,0 +1,3 @@
+"""
+file to process video to frames
+"""
