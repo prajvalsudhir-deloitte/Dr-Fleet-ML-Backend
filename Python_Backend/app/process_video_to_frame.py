e094189939be3ffa9da1899f1b6b002ecd4a9b45
krpancholi <101543829+KritikDeloitte@users.noreply.github.com>
2022-10-31 15:50:45 +0530

    Video to frames with opencv

--- Python_Backend/app/process_video_to_frame.py
+++ Python_Backend/app/process_video_to_frame.py
@@ -1,3 +1,35 @@
-"""
-file to process video to frames
-"""
+import cv2
+  
+# Function to extract frames
+def FrameCapture(path):
+    
+    try:
+    # Path to video file
+        vidObj = cv2.VideoCapture(path)
+        
+        if (vidObj.isOpened()== False): 
+            print("Error opening video stream or file")
+            
+        # Used as counter variable
+        count =0
+    
+        while vidObj.isOpene():
+    
+            # vidObj object calls read
+            # function extract frames
+            success, frame = vidObj.read()
+
+            if success:
+            # Saves the frames with frame-count
+                cv2.imwrite("frame%d.jpg" % count, frame)
+
+                # TODO : can pass frame to ML model 
+                count += 1
+            else: 
+                print("video completed")
+                break
+        
+        # release the resourse 
+        vidObj.release()
+    except:
+        print("Error Occur")
